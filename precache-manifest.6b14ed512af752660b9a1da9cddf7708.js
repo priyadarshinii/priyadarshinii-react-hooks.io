8cbd602b21469d6693e19bb714cc1377ed151f1e
Priyadarshini Nadar <priyadarshinignadar@gmail.com>
2020-03-17 23:27:34 +0530

    Add files via upload

---
+++ precache-manifest.6b14ed512af752660b9a1da9cddf7708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19402d74291c7543ce47902c23daba1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b08a3d741cb6d419f2f",
+    "url": "/static/css/main.021f4d9f.chunk.css"
+  },
+  {
+    "revision": "cec0a6c8b0d36ef4aa0b",
+    "url": "/static/js/2.03609c79.chunk.js"
+  },
+  {
+    "revision": "5b08a3d741cb6d419f2f",
+    "url": "/static/js/main.81641b8c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
